7947b23930d60bbd3e446b17764473c983fcaa4a
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-02-12 09:54:54 +0900

    hooks_riverpodに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
 // プロバイダを使うことで値のモックやオーバーライドが可能になります。
