64a040a6322178b0fe33821d802ea76efff04cfa
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-02-12 20:21:26 +0900

    stateProvider、statenotierのstateプロパティはデフォルトで用意されている。

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,121 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:math';
 
-// 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
-// プロバイダを使うことで値のモックやオーバーライドが可能になります。
-// Providerの定数をグローバルに宣言
-final counterProvider = StateProvider((ref) => 0);
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_countup/todo.dart';
+
+// Provider example.
+final todoListNotifierProvider =
+    StateNotifierProvider<TodoListNotifier, List<Todo>>((ref) {
+  return TodoListNotifier();
+});
+
+class TodoListNotifier extends StateNotifier<List<Todo>> {
+  TodoListNotifier()
+      : super(const [
+          // サンプルのTodoを挿入
+          Todo(id: '1', title: 'Buy a coffee'),
+          Todo(id: '2', title: 'Buy a milk'),
+          Todo(id: '3', title: 'Eat sushi'),
+          Todo(id: '4', title: 'Build an sushi'),
+          Todo(id: '5', title: 'Build my app'),
+        ]);
+
+  /// 新しいTODOを追加するメソッド
+  void add(Todo todo) {
+    state = [...state, todo];
+  }
+
+  /// IDを指定して、TODOを削除するメソッド
+  void remove(String todoId) {
+    state = [
+      for (final todo in state)
+        if (todo.id != todoId) todo,
+    ];
+  }
+
+  /// IDを指定して、TODOの完了状態を切り替えるメソッド
+  void toggle(String todoId) {
+    state = [
+      for (final todo in state)
+        if (todo.id == todoId)
+          todo.copyWith(completed: !todo.completed)
+        else
+          todo,
+    ];
+  }
+}
 
 void main() {
   runApp(
     // プロバイダをウィジェットで利用するには、アプリ全体を
     // `ProviderScope` ウィジェットで囲む必要があります。
     // ここに各プロバイダのステート（状態）・値が格納されていきます。
-    ProviderScope(
-      child: MyApp(),
+    const ProviderScope(
+      child: StateNotifierProviderPage(),
     ),
   );
 }
 
-// StatelessWidget の代わりに Riverpod の ConsumerWidget を継承します。
-class MyApp extends HookConsumerWidget {
-  // `ConsumerWidget` と同じく、buildメソッドの引数に
-  // `WidgetRef ref` が追加され、使い方も一緒です。
+// Widget example.
+class StateNotifierProviderPage extends ConsumerWidget {
+  const StateNotifierProviderPage({
+    super.key,
+  });
+
+  static const String title = 'StateNotifierProvider';
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Providerを読み取る。 `.notifier` を付けると `StateController` が、
-    // 付けなければ、 `state` つまり int が取得できる。
-    final counter = ref.watch(counterProvider.notifier);
+    // StateNotifierProviderを読み取る。watchを使用しているので、
+    // state（状態）であるTODOリストが更新されると、buildメソッドが再実行されて画面が更新される
+    final todoList = ref.watch(todoListNotifierProvider);
+    // TodoListNotifier を使用する場合は `.notifier` を付けてProviderを読み取る
+    final notifier = ref.watch(todoListNotifierProvider.notifier);
+
+    // 新しいTodoを追加する、プライベートメソッドを定義
+    void addTodo() {
+      final newTodo = Todo(
+        id: Random().nextDouble().toString(),
+        title: DateTime.now().toIso8601String(),
+      );
+      notifier.add(newTodo);
+    }
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Example')),
-        body: ElevatedButton(
-          // ボタンタップでcounterProviderの状態をプラス１する
-          // ↓ `counter.state++` や、 `counter.state = counter.state + 1` と書いても同じ。
-          onPressed: () => counter.update((state) => state + 1),
-          // counterProviderの状態（カウント数）をTextで表示。値が変わると再描画される。
-          child: Text('Count: ${ref.watch(counterProvider)}'),
+        appBar: AppBar(
+          title: const Text(title),
+          actions: [
+            // ボタンを押して新しいTodoを追加できる（実際はTodoのタイトル等を入力できるようにすると良い）
+            IconButton(onPressed: addTodo, icon: const Icon(Icons.add))
+          ],
+        ),
+        body: SafeArea(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(16),
+            itemCount: todoList.length,
+            separatorBuilder: (_, __) => const SizedBox(height: 4),
+            itemBuilder: (context, index) {
+              final todo = todoList[index];
+              return Card(
+                child: ListTile(
+                  title: Text(todo.title),
+                  leading: Icon(
+                    todo.completed
+                        ? Icons.check_box
+                        : Icons.check_box_outline_blank,
+                  ),
+                  trailing: TextButton(
+                    onPressed: () => notifier.remove(todo.id),
+                    child: const Text('Delete'),
+                  ),
+                  // タップでTODOの完了状態を切り替える
+                  onTap: () => notifier.toggle(todo.id),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
