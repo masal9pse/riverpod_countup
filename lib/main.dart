ba454219f8212c4ec9ba1585ba387045d45d2406
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-02-12 10:25:34 +0900

    hooks_riverpodはHookConsumerWidgetを使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
 // プロバイダを使うことで値のモックやオーバーライドが可能になります。
-final helloWorldProvider = Provider((_) => 'Hello world');
+final appNameProvider = Provider((ref) => 'Special App!');
 
 void main() {
   runApp(
@@ -17,16 +18,22 @@ void main() {
 }
 
 // StatelessWidget の代わりに Riverpod の ConsumerWidget を継承します。
-class MyApp extends ConsumerWidget {
+class MyApp extends HookConsumerWidget {
+  // `ConsumerWidget` と同じく、buildメソッドの引数に
+  // `WidgetRef ref` が追加され、使い方も一緒です。
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
+    // `ref.watch` を使用して Providerを読み取ります。
+    final String value = ref.watch(appNameProvider);
+    // `HookConsumerWidget` を継承しているので `useXxx` メソッドが使用できる。
+    final counter = useState(0);
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
+        appBar: AppBar(title: Text('Example')),
         body: Center(
-          child: Text(value),
+          // Providerから読み取った値と、 `useState` の値を使用する例
+          child: Text('$value, ${counter.value}'),
         ),
       ),
     );
