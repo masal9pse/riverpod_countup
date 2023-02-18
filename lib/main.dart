536686815e5d44572370be4be9fb4134d015c50f
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-02-18 13:12:27 +0900

    statenotiferを使ってカウントアプリを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_countup/todo.dart';
 
-final counterProvider = StateProvider<int>((ref) => 0);
+// final counterProvider = StateProvider<int>((ref) => 0);
+
+@immutable
+class CountModel {
+  const CountModel({required this.count});
+  final int count;
+
+  CountModel copyWith({required int count}) {
+    return CountModel(count: count);
+  }
+}
+
+class CountNotifier extends StateNotifier<CountModel> {
+  CountNotifier() : super(const CountModel(count: 0));
+
+  void addCount() {
+    state = state.copyWith(count: state.count + 1);
+  }
+}
+
+final countProvider = StateNotifierProvider<CountNotifier, CountModel>((ref) {
+  return CountNotifier();
+});
 
 void main() {
   runApp(
@@ -23,29 +42,23 @@ class StateNotifierProviderPage extends ConsumerWidget {
     super.key,
   });
 
-  static const String title = 'StateNotifierProvider';
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // StateNotifierProviderを読み取る。watchを使用しているので、
-    // state（状態）であるTODOリストが更新されると、buildメソッドが再実行されて画面が更新される
-    var counter = ref.watch(counterProvider);
+    var counter1 = ref.watch(countProvider).count;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          title: const Text('title'),
           actions: [],
         ),
         body: ElevatedButton(
           onPressed: () {
-            // counter++;
-            // ref.read(counterProvider.notifier).state++;
             // updateを使う書き方もできる。
-            // ref.read(counterProvider.notifier).update((state) => state ++);
-            ref.read(counterProvider.notifier).update((state) => state + 1);
+            ref.read(countProvider.notifier).addCount();
           },
-          child: Text(counter.toString()),
+          child: Text(counter1.toString()),
         ),
       ),
     );
